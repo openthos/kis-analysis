6fda14280602a69947eeea3a864b248f2a259f19
Junjie Mao <junjie.mao@intel.com>
2016-01-14 15:18:39 +0800

    Add completions to lkp
    Signed-off-by: Junjie Mao <junjie.mao@intel.com>

--- envsetup.sh
+++ envsetup.sh
@@ -2,3 +2,24 @@
 
 export LKP_SRC=$PWD
 export PATH=$PATH:$LKP_SRC/bin
+
+_lkp() {
+	local cur=${COMP_WORDS[COMP_CWORD]}
+	local prev=${COMP_WORDS[COMP_CWORD-1]}
+	COMPREPLY=()
+
+	case "$prev" in
+		lkp)
+			bins=$(ls $LKP_SRC/bin)
+			sbins=$(ls $LKP_SRC/sbin)
+			lkp_execs=$(ls $LKP_SRC/lkp-exec)
+			tools=$(ls $LKP_SRC/tools)
+			completions="$bins $sbins $lkp_execs $tools"
+			COMPREPLY=( $( compgen -W "$completions" -- $cur ))
+			;;
+	esac
+
+	return 0
+}
+
+complete -o default -F _lkp lkp
