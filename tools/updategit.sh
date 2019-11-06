04e9cf28071c8c3dd73bb06cc7f4c3c1d63db77c
Midysen <13250056+Midysen@users.noreply.github.com>
2019-11-06 10:42:13 +0800

    Add files via upload

--- tools/updategit.sh
+++ tools/updategit.sh
@@ -1,72 +1,104 @@
 #!/bin/bash
 
-dirname_path=$(cd `dirname $0`; pwd)
+function init_env() {
 
-#build_sh="$dirname_path/build.sh"
-test_sh="$dirname_path/test.sh"
-tmp_branch="$dirname_path/tmp_branch"
+	dirname_path=$(cd `dirname $0`; pwd)
+	test_sh="$dirname_path/test.sh"
+	build="$dirname_path/build.sh"
+	tmp_branch="$dirname_path/tmp_branch"
 
-# for linux_repo NEED TO SET #linux_repo="/home/oto/thecode/cki/linux/"
-source $tmp_branch/envar
+	# for linux_repo NEED TO SET #linux_repo="/home/oto/thecode/cki/linux/"
+	source $tmp_branch/envar
+}
 
-cd $linux_repo
-# TBD: need get origin and master  from git remote -v
-git fetch origin master
+function init_git_env() {
+	if [ ! -d $linux_repo ]
+	then
+		mkdir -p $linux_repo
+	fi
+	cd $linux_repo
+	# TBD: need get origin and master  from git remote -v
+	git init
+	git config --global user.name "Midysen"
+	git config --global user.email "miaodexing@126.com"
+	git remote add origin http://192.168.0.99/root/linux.git
+	git fetch origin master
 
-if [ $? -ne 0 ]; then
-   echo -e "git fetch ERROR!"
-   exit
-fi  
+	if [ $? -ne 0 ]; then
+		echo -e "git fetch ERROR!"
+		exit
+	fi  
+}
 
-#remote_branches=$(git branch -r | sed -e  "s/origin\///")
+function branch_log() {
+	branch_log=$(git log --pretty=format:"%H" --since=$1)
 
-echo "All branches: ($remote_branches)"
+	echo -e "NEW info:: Branch $br_: ($branch_log)"
 
-remote_branches="origin/master" #remote_branches="origin/master origin/HEAD"
-echo $remote_branches
+	for log_ in $branch_log
+	do 
+		echo '#!/bin/bash' > $tmp_branch/$log_
+		echo -e "/bin/bash $build $br_ $log_" >> $tmp_branch/$log_
+		#echo -e "/bin/bash $test_sh $br_ $log_" >> $tmp_branch/$log_
+		chmod 755 $tmp_branch/$log_
+		echo -e "\033[32m Running $br_ $tmp_branch/$log_ \033[0m"
+		/bin/bash $tmp_branch/$log_
 
-for br_ in $remote_branches
-do 
+		sleep 2
+	done
 
-echo $br_
-nbr_=${br_/\//##}
-echo $nbr_
+}
 
-#old_br_com : old_branch_commit id
-old_br_com=(`cat $tmp_branch/$nbr_`)
-echo $old_br_com
+function remote_branch() {
+	#remote_branches=$(git branch -r | sed -e  "s/origin\///")
 
-git checkout $br_
+	git fetch origin master
 
-#new_br_com : new_branch_commit id
-new_br_com=(`git log -1 --pretty=format:"%H %cd" --date=raw`)
-echo $new_br_com
-if [ "${old_br_com[0]}"x = "${new_br_com[0]}"x ]
-then
-  echo No changes in $br_
-  continue
-else
-  echo New changes, need testing in $br_
-  #update newest old_branch_commit id
-  echo ${new_br_com[@]} > $tmp_branch/$nbr_
-fi
+	if [ $? -ne 0 ]; then
+		echo -e "git fetch ERROR!"
+		exit
+	fi
 
-branch_log=$(git log --pretty=format:"%H" --since=${old_br_com[1]} )
+	remote_branches="origin/master" #remote_branches="origin/master origin/HEAD"
+	echo $remote_branches
 
-echo -e "NEW info:: Branch $br_: ($branch_log)"
+	for br_ in $remote_branches
+	do 
 
-for log_ in $branch_log
-do 
-	echo '#!/bin/bash' > $tmp_branch/$log_
-	#echo -e "/bin/bash $build_sh $br_ $log_" >> $tmp_branch/$log_
-        echo -e "/bin/bash $test_sh $br_ $log_" >> $tmp_branch/$log_
-	chmod 755 $tmp_branch/$log_
-	echo -e "\033[32m Running $br_ $tmp_branch/$log_ \033[0m"
-	/bin/bash $tmp_branch/$log_
+		echo $br_
+		nbr_=${br_/\//##}
+		echo $nbr_
 
-	sleep 2
+		#old_br_com : old_branch_commit id
+		old_br_com=(`cat $tmp_branch/$nbr_`)
+		echo $old_br_com
+
+		git checkout $br_
+
+		#new_br_com : new_branch_commit id
+		new_br_com=(`git log -1 --pretty=format:"%H %cd" --date=raw`)
+		echo $new_br_com
+		if [ "${old_br_com[0]}"x == "${new_br_com[0]}"x ]
+		then
+			echo No changes in $br_
+			continue
+		else
+			echo New changes, need testing in $br_
+			#update newest old_branch_commit id
+			echo ${new_br_com[@]} > $tmp_branch/$nbr_
+		fi
+
+		branch_log ${old_br_com[1]}
+		#for loop
+	done
+}
+
+
+init_env
+
+while(true)
+do
+	init_git_env
+	remote_branch
+	sleep  5
 done
-
-#for loop
-done
-
