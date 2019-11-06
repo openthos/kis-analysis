35438b61f10b9e8c7fb36abbcbfa80cdb6d11745
Midysen <13250056+Midysen@users.noreply.github.com>
2019-11-06 10:47:00 +0800

    Add files via upload

--- tools/build.sh
+++ tools/build.sh
@@ -1,82 +1,82 @@
 #!/bin/bash
 
-unset LD_LIBRARY_PATH
-dirname_path=$(cd `dirname $0`; pwd)
-
-buildroot_path="$dirname_path/buildroot"
-tmp_branch="$dirname_path/tmp_branch"
-
-source $tmp_branch/envar
-#linux_repo="/c/repo/linux"
-#linux_repo="/media/vdb1/xly/common"
-#linux_config="$linux_repo/arch/x86/configs/x86_64_defconfig"
-
-#tmp_branch="/home/chy/xly/kernelci/ka/tmp_branch"
 branch_name="$1"
 commit_id="$2"
 
-#buildroot_path="/home/chy/xly/kernelci/ka/buildroot"
-buildroot_linux="$buildroot_path/output/build/linux-custom"
-buildroot_mod="$buildroot_path/output/MOD"
-buildroot_config="$buildroot_path/buildroot_config2"
+function init_env() {
+	dirname_path=$(cd `dirname $0`; pwd)
+	tmp_branch="$dirname_path/tmp_branch"
 
-linux_config="$buildroot_path/linux_config"
+	source $tmp_branch/envar
+	buildroot_linux=$linux_repo
+}
 
-#web_path="/var/www/html/~lkp"
+function init_git(){
+	cd $buildroot_linux
 
-cd $linux_repo
+	git checkout $branch_name
+	git checkout $commit_id
 
-#git checkout $branch_name
+	if [ $? -ne 0 ]; then
+		echo -e "git checkout $commit_id ERROR!"
+		exit
+	fi
 
-git checkout $commit_id 
+}
 
-        if [ $? -ne 0 ]; then
-        echo -e "git checkout $commit_id ERROR!"
-        exit
-        fi  
+function init_kernel_config(){
+	cp $buildroot_linux/arch/x86/configs/x86_64_defconfig  $buildroot_linux/.config
+}
 
-#echo -e "Syncing from $linux_repo to $buildroot_linux"
-#rsync -au --chmod=u=rwX,go=rX --exclude .svn --exclude .git --exclude .hg --exclude .bzr --exclude CVS --exclude .stamp_rsynced $linux_repo/ $buildroot_linux
+function build_kernel(){
+	make x86_64_defconfig
+	make
 
-rm -rf $buildroot_linux $buildroot_mod
+	if [ $? -ne 0 ]; then
+		echo -e "make ERROR!"
+		exit
+	fi
+}
 
-cp $buildroot_config $buildroot_path/.config
+function output_log(){
+	echo -e "$sec $branch_name $commit_id --> (${!file_time[@]})" >> $tmp_branch/build.log
+	echo -e "$branch_name \033[32m$commit_id\033[0m output files: (\033[32m${!file_time[@]} \033[0m) in $image_path"
+}
 
-echo -e "
-WEB_PATH=\"$web_path\"
-BRANCH_NAME=\"$branch_name\"
-COMMIT_ID=\"$commit_id\"
-BR2_DEFCONFIG=\"$buildroot_path/configs/qemu_x86_64_defconfig\"
-BR2_LINUX_KERNEL_CUSTOM_LOCAL_PATH=\"$linux_repo\"
-BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE=\"$linux_config\"" >> $buildroot_path/.config
+function images_commit(){
+	cd $buildroot_linux/arch/x86_64/boot/
+	declare -A file_time
+	sec=$(date +%s)
+	for file_ in $(ls)
+	do
+		time_=$(stat -c %Y $file_)
+		if [ $time_ -gt $sec ]; then
+			file_time[$file_]=$time_
 
-#exit
-
-cd $buildroot_path
-sec=$(date +%s)
-
-make -j8
-
-        if [ $? -ne 0 ]; then
-        echo -e "make ERROR!"
-        exit
-        fi  
-
-cd $buildroot_path/output/images
-
-declare -A file_time
-for file_ in $(ls)
-do
-	time_=$(stat -c %Y $file_)
-        if [ $time_ -gt $sec ]; then
-	file_time[$file_]=$time_
-
-		if [ "$file_"x = "bzImage"x ]; then
-		rm $tmp_branch/$commit_id
+			if [ "$file_"x == "bzImage"x ]; then
+				rm  $tmp_branch/$commit_id
+			fi
 		fi
-        fi  
-done
+	done
+	image_path=$dirname_path/buildroot/output/images
+	if [ ! -d $image_path ]
+	then
+		mkdir -p $image_path
+	fi
+	cp bzImage $image_path
+	output_log
+}
 
-echo -e "$sec $branch_name $commit_id --> (${!file_time[@]})" >> $tmp_branch/build.log
-echo -e "$branch_name \033[32m$commit_id\033[0m output files: (\033[32m ${!file_time[@]} \033[0m) in $buildroot_path/output/images"
 
+function test_image() {
+	echo $dirname_path
+	cd $dirname_path
+	./test.sh   $image_path/bzImage   rootfs.ext2
+}
+
+init_env
+init_git
+init_kernel_config
+build_kernel
+images_commit
+test_image
--- tools/test.sh
+++ tools/test.sh
@@ -1,1 +1,5 @@
-echo test!!!
+#!/bin/bash
+
+#qemu-system-x86_64 -enable-kvm -m 1G -append "root=/dev/sda rdinit=/sbin/init" -serial stdio -kernel $1 -hda $2
+echo "Test------------------------"
+qemu-system-x86_64  -m 2G -append "root=/dev/sda rdinit=/sbin/init" -serial stdio -kernel $1  -hda $2
